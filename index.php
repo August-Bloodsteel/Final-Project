2fc7a5b1eb42afe2931a2cfc81541146138cc9dd
August-Bloodsteel <augustbloodsteel@gmail.com>
2023-05-01 04:49:19 +0800

    First Step

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+    $koneksi = mysqli_connect('sql12.freemysqlhosting.net','sql12615102','g3EA87C6WL','sql12615102');
+
+    if($koneksi){
+        echo'Berhasil';
+    }else{
+        echo'gagal';
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
